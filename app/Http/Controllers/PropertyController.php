ce0fbc9ea5b2ee9e01dca4ec4e01e0ca703ad648
ilkin <i.fleydanli@gmail.com>
2020-07-14 06:42:57 +0300

    Added status codes and exception catching in PropertyController.php

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AssignAnalyticToPropertyRequest;
+use App\Http\Requests\CreatePropertyRequest;
+use App\Repositories\Interfaces\PropertyAnalyticRepositoryInterface;
 use App\Repositories\Interfaces\PropertyRepositoryInterface;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PropertyController extends Controller
 {
@@ -17,8 +23,72 @@ class PropertyController extends Controller
         $this->propertyRepository = $propertyRepository;
     }
 
-    public function index()
+
+    /**
+     * @param Request $request
+     * @param PropertyAnalyticRepositoryInterface $propertyAnalyticRepository
+     * @return JsonResponse
+     */
+    public function index(
+        Request $request,
+        PropertyAnalyticRepositoryInterface $propertyAnalyticRepository
+    ): JsonResponse {
+        $ids = $this->propertyRepository->getIdsByCondition($request->all());
+
+        return response()->json($propertyAnalyticRepository->getSummary($ids), Response::HTTP_OK);
+    }
+
+    /**
+     * @param CreatePropertyRequest $request
+     * @return JsonResponse|object
+     */
+    public function create(CreatePropertyRequest $request): JsonResponse
     {
-        return $this->propertyRepository->getAnalytic(1);
+        return response()->json($this->propertyRepository->create($request->all()), Response::HTTP_OK);
+    }
+
+    /**
+     * @param $id
+     * @param AssignAnalyticToPropertyRequest $request
+     * @return JsonResponse
+     */
+    public function assignAnalytic(int $id, AssignAnalyticToPropertyRequest $request): JsonResponse
+    {
+        try {
+            $this->propertyRepository->attachAnalytic($request->all(), $id);
+
+            return response()->json('Success!', Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * @param int $id
+     * @param AssignAnalyticToPropertyRequest $request
+     * @return JsonResponse
+     */
+    public function update(int $id, AssignAnalyticToPropertyRequest $request): JsonResponse
+    {
+        try {
+            $this->propertyRepository->updateAttachedAnalytic($request->all(), $id);
+
+            return response()->json('Success!', Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show(int $id): JsonResponse
+    {
+        try {
+            return response()->json($this->propertyRepository->getAnalytic($id), Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), $e->getCode());
+        }
     }
 }
